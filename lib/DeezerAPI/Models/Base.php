09a6b69586bc57e4d12c76ea94706dc508c742e4
Arnaud <arnaud.costes@gmail.com>
2016-10-16 21:40:33 +0200

    Revert "Fix Get track by ID"

--- lib/DeezerAPI/Models/Base.php
+++ lib/DeezerAPI/Models/Base.php
@@ -83,10 +83,7 @@ abstract class Base {
      * Make a request to the API to get the object
      */
     protected function _get() {
-        $class = explode('\\', get_called_class());
-        $class = end($class);
-        $url = Search::DEEZER_API_URL . '/' . strtolower($class) . '/' . $this->id;
-        $this->_data = json_decode(file_get_contents($url));
+        $this->_data = json_decode(file_get_contents(Search::DEEZER_API_URL . '/' . strtolower(get_called_class()) . '/' . $this->id));
     }
 
     /**
